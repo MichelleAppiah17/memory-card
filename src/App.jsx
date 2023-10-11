6d9d9001625ca2226ec8db70a33f1a95ec3ff09d
Michelle Appiah <michappiah67@gmail.com>
2023-10-11 22:34:29 +0000

    corrected component import

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import GameBoard from './components/gameBoard'
+import {GameBoard} from './components/GameBoard'
 
 function App() {
  
